4c7e67089a17efbb203af02138ccd6b512ea212a
Vincent Berthier <vincent.berthier@posteo.org>
2025-08-16 16:47:13 +0200

    add duplicacy setup

--- post_install/user_setup.sh
+++ post_install/user_setup.sh
@@ -53,3 +53,39 @@ FISH_SCRIPT_EOF
     
     print_success "Fish plugins installed"
 }
+
+setup_duplicacy_config() {
+    local folders=("$HOME/code" "$HOME/Documents" "$HOME/Images" "$XDG_CONFIG_HOME")
+    for folder in "${folders[@]}"; do
+        basename=$(basename "$folder")
+        # Remove leading dot if present
+        basename=${basename#.}
+        # Convert to lowercase
+        basename=$(echo "$basename" | tr '[:upper:]' '[:lower:]')
+        # Append hostname if basename is 'config'
+        if [[ "$basename" == "config" ]]; then
+            basename="${basename}_$(hostname)"
+        fi
+        # Append /.duplicacy
+        duplicacy_path="${basename}/.duplicacy"
+
+        mkdir -p "$duplicacy_path"
+        cat > "${duplicacy_path}/preferences" << DUP_EOF
+[
+    {
+        "name": "default",
+        "id": "${basename}",
+        "repository": "",
+        "storage": $HOME/vault",
+        "encrypted": true,
+        "no_backup": false,
+        "no_restore": false,
+        "no_save_password": false,
+        "nobackup_file": "",
+        "keys": null,
+        "filters": "${HOME}/.config/duplicacy/filters.txt",
+        "exclude_by_attribute": false
+    }
+]}
+DUP_EOF
+    done
